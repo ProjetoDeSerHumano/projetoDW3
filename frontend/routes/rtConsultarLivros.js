ed810fa95d6ef176c3a50ac45b8794a1dbf6487f
Heitor Pinati <heitorpinatimartins@gmail.com>
2025-11-17 22:57:34 -0300

    frontend

---
+++ frontend/routes/rtConsultarLivros.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var livrosConsultarApp = require("../apps/livros/controller/ctlConsultarLivros");
+
+function authenticationMiddleware(req, res, next) {
+    const isLogged = req.session.isLogged;
+    
+    
+    if (!isLogged) {
+        return res.redirect("/Login");
+    }
+    
+    
+    next();
+}
+
+
+router.get('/', authenticationMiddleware, (req, res) => {
+    
+    return res.redirect('/livros/ConsultarLivros');
+
+});
+
+
+router.get('/ConsultarLivros', authenticationMiddleware, livrosConsultarApp.consultarLivros); 
+/*router.get('/InsertContas', authenticationMiddleware, contasApp.insertContas); 
+router.get('/ViewConta/:id', authenticationMiddleware, contasApp.ViewConta); 
+router.get('/UpdateConta/:id', authenticationMiddleware, contasApp.UpdateConta); 
+
+router.post('/InsertContas', authenticationMiddleware, contasApp.insertContas); 
+router.post('/UpdateContas', authenticationMiddleware, contasApp.UpdateConta); 
+router.post('/DeleteContas', authenticationMiddleware, contasApp.DeleteConta); */
+
+module.exports = router;
